42ee7eb365bc1a65dc2b8bd9a3f186e05fdd2b20
MysticGohu <49130469+MysticGohu@users.noreply.github.com>
2019-03-31 23:36:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token; // a garder en version heroku
-const prefix = ("?");
+const prefix = (cfg.prefix);
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
 });
 
-bot.login(cfg.token); //a garder en version desktop
+
+
 bot.login(token); //a garder en version heroku
